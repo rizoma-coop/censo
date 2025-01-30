53d37b5f1d428eaa0b526da20b43a087b6f149a5
esaramago <dev@emanuelsaramago.com>
2025-01-30 14:48:17 +0000

    sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import netlify from '@astrojs/netlify'
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [
-    vue()
-  ]
+  integrations: [vue(), sentry({
+    dsn: 'https://646a1ed8c1e8c353250febd5c6cd90df@o4508733140959232.ingest.de.sentry.io/4508733143842896',
+    sourceMapsUploadOptions: {
+      project: 'javascript-astro',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  })]
 })
